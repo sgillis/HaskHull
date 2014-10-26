a83523b20801cbe977c3cbbe92de71835ac6b791
San Gillis <gillis.san@gmail.com>
2014-10-26 11:49:37 +0100

    Make points object

--- src/Objects/Points.hs
+++ src/Objects/Points.hs
@@ -1,6 +1,7 @@
 module Objects.Points (makePoints) where
 
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Maybe (MaybeT)
 
 import qualified Graphics.Rendering.OpenGL as GL
 
@@ -8,8 +9,25 @@ import Paths_HaskHull
 import Graphics.Object (makeObject, Object)
 import Graphics.Buffers (ptrOffset)
 
-makePoints :: IO ()
+makePoints :: MaybeT IO Object
 makePoints = do
-    vertShaderPath <- liftIO $ getDataFileName "points_vertex.shader"
-    fragShaderPath <- liftIO $ getDataFileName "points_fragment.shader"
-    return ()
+    vertShaderPath <- liftIO $ getDataFileName "vertex.shader"
+    fragShaderPath <- liftIO $ getDataFileName "fragment.shader"
+    makeObject
+        [ (GL.VertexShader, vertShaderPath)
+        , (GL.FragmentShader, fragShaderPath)
+        ]
+        vertexPositions
+        [("position", GL.VertexArrayDescriptor 4 GL.Float 0 $ ptrOffset 0)]
+        GL.Points 8
+    where
+        vertexPositions =
+            [  1.0,  1.0,  1.0, 1.0
+            ,  1.0,  1.0, -1.0, 1.0
+            ,  1.0, -1.0,  1.0, 1.0
+            ,  1.0, -1.0, -1.0, 1.0
+            , -1.0,  1.0,  1.0, 1.0
+            , -1.0,  1.0, -1.0, 1.0
+            , -1.0, -1.0,  1.0, 1.0
+            , -1.0, -1.0, -1.0, 1.0
+            ]
