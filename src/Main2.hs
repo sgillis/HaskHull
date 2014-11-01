84cb8fa74d13872ef9995247beb0a03b285c1f0c
San Gillis <gillis.san@gmail.com>
2014-11-01 16:38:08 +0100

    Parse points

--- src/Main2.hs
+++ src/Main2.hs
@@ -1,30 +1,59 @@
 import Text.ParserCombinators.Parsec
 import Data.List
+import Control.Applicative hiding ((<|>), many)
 
 import Paths_HaskHull
 
-csvFile :: Parser [[String]]
-csvFile = endBy line eol
+data Point = Point
+    { x :: Double
+    , y :: Double
+    , z :: Double
+    } deriving Show
 
-line :: Parser [String]
-line = sepBy cell (char ',')
+csvFile :: Parser [Point]
+csvFile = endBy point eol
 
-cell :: Parser [Char]
-cell = spaces >> many (noneOf ",\n")
+point :: Parser Point
+point = Point <$> double <*> (char ',' *> double) <*> (char ',' *> double)
+
+line :: Parser [Double]
+line = sepBy double (char ',')
 
 eol :: Parser Char
 eol = char '\n'
 
-parseCSV :: String -> Either ParseError [[String]]
-parseCSV input = parse csvFile "" input
+(<++>) :: Applicative f => f [a] -> f [a] -> f[a]
+(<++>) a b = (++) <$> a <*> b
+
+(<:>) :: Applicative f => f a -> f [a] -> f [a]
+(<:>) a b = (:) <$> a <*> b
+
+plus :: Parser String
+plus = char '+' *> number
+
+minus :: Parser String
+minus = char '-' <:> number
+
+number :: Parser String
+number = many1 digit
+
+integer :: Parser String
+integer = plus <|> minus <|> number
+
+double :: Parser Double
+double = read <$> (spaces *> integer <++> decimal)
+    where decimal = option "" $ char '.' <:> number
+
+parseCSV :: String -> Either ParseError [Point]
+parseCSV = parse csvFile ""
 
 readInputFile :: String -> IO String
 readInputFile name =
     getDataFileName name >>= readFile
 
 main :: IO ()
-main =
-    readInputFile "points.txt" >>= \input ->
+main = do
+    input <- readInputFile "points.txt"
+    let parseResult = parseCSV input
     either (\error -> putStrLn "something went wrong")
-           (\input -> putStrLn $ intercalate "|" $ concat input)
-           (parseCSV input)
+           print parseResult
